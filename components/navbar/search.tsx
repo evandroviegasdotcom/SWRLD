e5f1d1f6f37af8f8af505e8b1bced167be2195b9
Evandro Viegas <evandroviegas.com@gmail.com>
2024-12-11 15:44:58 +0000

    init

---
+++ components/navbar/search.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Input } from '../ui/input'
+
+export default function Search() {
+    const handleSearch = async (formData:FormData) => {
+        "use server"
+    }
+  return (
+    <form action={handleSearch}>
+        <Input 
+        type='search'
+        name='search'
+        placeholder='Search...'
+        className='w-[300px]'
+        />
+    </form>
+  )
+}
